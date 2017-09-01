cf99fe244c6736178d70723ce4a3537eaff3d86a
Matt Huntsberry <mmhuntsberry@Matts-MacBook.local>
2017-09-01 15:45:17 -0500

    1st commit --> fetchProject

--- script.jsx
+++ script.jsx
@@ -7,12 +7,36 @@ const styles = require('./style.css');
 class Fetch extends React.Component {
   constructor(props) {
     super(props);
-  }
+    this.state = {
+      countries: []
 
-  render() {
+    };
+  }
+  componentDidMount () {
+    fetch('http://services.groupkt.com/country/get/all').then((response) => {
+      response.json().then((data) => {
+        const countries = data.RestResponse.result;
+        this.setState({countries});
+        console.log(countries);
+      });
+    });
+  }
+  render () {
     return (
-      <h1>Address Book / Country Codes</h1>
-      // Your code here
+      <table className='myTable'>
+        <tr>
+          <th>Country</th>
+          <th>Alpha 2 Code</th>
+          <th>Alpha 3 Code</th>
+        </tr>
+        {this.state.countries.map(country =>
+          <tr key={country.alpha3_code}>
+            <td>{country.name}</td>
+            <td className='alpha'>{country.alpha2_code}</td>
+            <td className='alpha'>{country.alpha3_code}</td>
+          </tr>
+        )}
+      </table>
     );
   }
 }
